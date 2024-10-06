3dcf73fd42bfbf9b6473127602b1b33386feee86
Hyunjiiing <h.jilluminate@gmail.com>
2024-10-06 16:36:40 +0900

    Create basic StorePage layout with AppBar

--- lib/screens/storePage.dart
+++ lib/screens/storePage.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 class storePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Color(0xFFCBE3FA),
+        elevation: 0,
+        title: Text(
+          '상점',
+          style: TextStyle(
+              fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context); // 뒤로 가기
+          },
+        ),
+      ),
+
+    );
   }
+
 }
