254b3e4d1a056de281e503674b3b6d9bca21c3d1
Hyunjiiing <h.jilluminate@gmail.com>
2024-10-06 17:47:17 +0900

    Create basic RecorderGamePage layout

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:soom_charm/screens/mini_game/mini_game_screen_test.dart';
 import 'package:soom_charm/screens/GPS_tracker_screen.dart';
+import '../mini_game/components/recorderGamePage.dart';
 
 class MainScreen extends StatelessWidget {
   @override
@@ -31,6 +32,15 @@ class MainScreen extends StatelessWidget {
                 );
               },
               child: Text('GPS tracker'),
+            ), SizedBox(height: 20), // 버튼 간 간격 조정
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => recorderGamePage()),
+                );
+              },
+              child: Text('RecorderGame'),
             ),
           ],
         ),
