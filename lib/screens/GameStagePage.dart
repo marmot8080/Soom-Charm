8059b1ef5f912434753385a2727d3c554ec05dca
marmot8080 <marmot8080@gmail.com>
2024-10-17 13:34:16 +0900

    Add HeartCounter

--- lib/screens/GameStagePage.dart
+++ lib/screens/GameStagePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:soom_charm/screens/mini_game/MiniGameTestPage.dart';
 import 'package:soom_charm/widgets/GameStageButton.dart';
 import 'package:soom_charm/screens/StorePage.dart';
+import 'package:soom_charm/widgets/HeartCounter.dart';
 
 class GameStagePage extends StatefulWidget {
   @override
@@ -9,6 +10,8 @@ class GameStagePage extends StatefulWidget {
 }
 
 class _GameStagePage extends State<GameStagePage> {
+  int _heartCount = 4;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,33 +33,7 @@ class _GameStagePage extends State<GameStagePage> {
                       icon: Icon(Icons.arrow_back, color: Colors.black, size: MediaQuery.of(context).size.height * 0.035)
                   ),
                   // 하트 아이콘들과 + 아이콘을 감싸는 컨테이너
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                    decoration: BoxDecoration(
-                      color: Color(0xFFCBE3FA), // 하늘색 배경
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Row(
-                      children: [
-                        Icon(Icons.favorite,
-                            color: Color(0xFFFF6D7A), size: 30), // 하트 색상 수정
-                        Icon(Icons.favorite,
-                            color: Color(0xFFFF6D7A), size: 30), // 하트 색상 수정
-                        Icon(Icons.favorite,
-                            color: Color(0xFFFF6D7A), size: 30), // 하트 색상 수정
-                        SizedBox(width: 4),
-                        IconButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => StorePage()),
-                              );
-                            },
-                            icon: Icon(Icons.add, color: Colors.black, size: 30)
-                        ),
-                      ],
-                    ),
-                  ),
+                  HeartCounter(heartCount: _heartCount)
                 ],
               ),
             ),
