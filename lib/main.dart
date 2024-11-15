bd2277994d71e6f8ced05cc5dd954e682ac6ecfa
marmot8080 <marmot8080@gmail.com>
2024-11-15 11:27:26 +0900

    Add test code for SharedPreferenceManager

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
-import 'screens/main_screen.dart';
+import 'package:soom_charm/util/SharedPreferenceManager.dart';
+import 'package:soom_charm/screens/main_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget{
+  @override
+  _MyApp createState() => _MyApp();
+}
+
+class _MyApp extends State<MyApp> {
+  late SharedPreferenceManager spManager;
+
+  @override
+  void initState() {
+    super.initState();
+
+    spManager = SharedPreferenceManager();
+    spManager.initInstance();
+
+    spManager.setHeartCount(6);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
