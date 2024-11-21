6768e74f2f6b90f70bc91afa6bb7393ba95682bc
Hyunjiiing <h.jilluminate@gmail.com>
2024-11-21 15:56:49 +0900

    Fix StorePage.dart

--- lib/screens/StorePage.dart
+++ lib/screens/StorePage.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:soom_charm/screens/MainPage.dart';
+import 'package:shared_preferences/shared_preferences.dart'; // SharedPreferences 추가
 
-class StorePage extends StatelessWidget {
+class StorePage extends StatefulWidget {
+  @override
+  _StorePageState createState() => _StorePageState();
+}
+
+class _StorePageState extends State<StorePage> {
+  int point = 0; // 포인트 초기값
+
+  @override
+  void initState() {
+    super.initState();
+    _initializePoint();
+  }
+
+  Future<void> _initializePoint() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      point = prefs.getInt('point') ?? 0; // SharedPreferences에서 포인트 로드
+    });
+  }
+
+  Future<void> _buyHeart(int heartCount, int price) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    int currentPoint = prefs.getInt('point') ?? 0;
+    if (currentPoint >= price) {
+      prefs.setInt('point', currentPoint - price); // 포인트 차감
+
+      int currentHeartCount = prefs.getInt('heartCount') ?? 0;
+      prefs.setInt('heartCount', currentHeartCount + heartCount); // 하트 추가
+
+      // 업데이트된 포인트 값 반영
+      setState(() {
+        point = currentPoint - price;
+      });
+    } else {
+      // 포인트 부족 시 알림
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('포인트가 부족합니다!')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,7 +54,10 @@ class StorePage extends StatelessWidget {
         title: Text(
           '상점',
           style: TextStyle(
-              fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
         ),
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.black),
@@ -25,29 +70,30 @@ class StorePage extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // 코인 보유량을 오른쪽에 배치하는 위젯
+            // 포인트 표시
             Row(
-              mainAxisAlignment: MainAxisAlignment.end, // 오른쪽 정렬
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Container(
                   padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                   decoration: BoxDecoration(
-                    color: Color(0xFFCBE3FA), // 하늘색 배경
+                    color: Color(0xFFCBE3FA),
                     borderRadius: BorderRadius.circular(20),
                   ),
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       Text(
-                        '5000', // 보유 코인 수
+                        '$point', // 동적으로 포인트 값 표시
                         style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold),
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                       SizedBox(width: 8),
                       Icon(Icons.monetization_on,
-                          color: Color(0xFFFFC648), size: 24), // 코인 아이콘
+                          color: Color(0xFFFFC648), size: 24),
                     ],
                   ),
                 ),
@@ -57,103 +103,76 @@ class StorePage extends StatelessWidget {
             // 상점 아이템 목록
             Expanded(
               child: GridView.count(
-                crossAxisCount: 2, // 한 줄에 두 개씩
-                crossAxisSpacing: 40, // 각 아이템 상자간의 가로 간격 늘리기
-                mainAxisSpacing: 0.5, // 각 아이템 상자간의 세로 간격 줄이기
-                childAspectRatio: 1.3, // 가로 세로 비율을 0.7로 설정하여 가로 길이 줄이기
+                crossAxisCount: 2,
+                crossAxisSpacing: 40,
+                mainAxisSpacing: 0.5,
+                childAspectRatio: 1.3,
                 children: [
-                  _buildItem(3, null, 300), // 하트 3개
-                  _buildItem(1, 5, 450), // 하트 1개 X 5
-                  _buildItem(1, 10, 900), // 하트 1개 X 10
-                  _buildItem(1, 15, 1350), // 하트 1개 X 15
-                  _buildItem(1, 20, 1800), // 하트 1개 X 20
-                  _buildItem(1, 25, 2000), // 하트 1개 X 25
+                  _buildItem(context, 3, 300),
+                  _buildItem(context, 5, 450),
+                  _buildItem(context, 10, 900),
+                  _buildItem(context, 15, 1350),
+                  _buildItem(context, 20, 1800),
+                  _buildItem(context, 25, 2000),
                 ],
               ),
             ),
             SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MainPage(initialIndex: 0,)), // GPSTrackerPage로 이동
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                backgroundColor: Color(0xFFCBE3FA),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              child: Text(
-                '돈벌러 가기',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
           ],
         ),
       ),
     );
   }
 
-  // 상점 아이템 위젯
-  Widget _buildItem(int heartCount, int? multiple, int price) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        // 아이템 박스
-        Container(
-          padding: EdgeInsets.symmetric(
-              vertical: 30, horizontal: 30), // 상자 내부 세로 여백 증가
-          width: 160,
-          decoration: BoxDecoration(
-            color: Color(0xFFCBEFFA), // 색상을 CBEFFA로 변경
-            borderRadius: BorderRadius.circular(20), // 모서리를 더 둥글게
-          ),
-          child: Column(
-            children: [
-              // 하트 아이콘 생성
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ...List.generate(
-                    heartCount, // heartCount만큼 하트 아이콘 생성
-                    (index) => Icon(Icons.favorite,
-                        color: Color(0xFFFF6D7A), size: 24),
-                  ),
-                  if (multiple != null) // X와 숫자 텍스트 추가
-                    Padding(
-                      padding: const EdgeInsets.only(left: 4.0),
-                      child: Text(
-                        'X $multiple',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
+  Widget _buildItem(BuildContext context, int heartCount, int price) {
+    return GestureDetector(
+      onTap: () async {
+        await _buyHeart(heartCount, price); // 하트 구매 후 포인트 업데이트
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('$heartCount개의 하트를 구매했습니다!')),
+        );
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+            width: 160,
+            decoration: BoxDecoration(
+              color: Color(0xFFCBEFFA),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.favorite, color: Color(0xFFFF6D7A), size: 24),
+                    SizedBox(width: 4),
+                    Text(
+                      'X $heartCount',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
-                ],
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.monetization_on,
+                  color: Color(0xFFFFC648), size: 24),
+              SizedBox(width: 4),
+              Text(
+                '$price',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
             ],
           ),
-        ),
-        SizedBox(height: 8), // 간격 추가
-        // 가격을 표시하는 부분
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.monetization_on,
-                color: Color(0xFFFFC648), size: 24), // 코인 아이콘
-            SizedBox(width: 4),
-            Text(
-              '$price',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
